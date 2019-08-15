a23f24bfc62b8f51b700e205d14b32bcc09195e0
Duong Dang <dddangmedia@gmail.com>
2019-08-15 06:02:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ function whisper(string) {
 function logShout(string) {
   return console.log(string.toUpperCase())
 }
+
+function logWhisper(string) {
+  return console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  returns if(string === 'hello!') {
+    console.log("I can't hear you!")
+  }
+}
